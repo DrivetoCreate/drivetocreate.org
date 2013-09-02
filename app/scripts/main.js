7e2970dff58e50a7cc70638035afbe555b270f82
Will Dages <wdages@gmail.com>
2013-09-02 14:47:03 -0400

    Stopping point for homepage work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,12 @@
 $(document).foundation();
 
 fitText(document.getElementById('hp-dollars-donated'), 0.72)
+
+var vimeoVideoId = '50959272'
+var vimeoPlayer = '<iframe src="http://player.vimeo.com/video/' + vimeoVideoId + '" frameborder="0" class="vimeo-video" autoplay webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
+
+$("dark-overlay, .play-button, .close-overlay, .video-embed").on('click', function () {
+	$(".video-embed").html($(vimeoPlayer));
+	$(".dark-overlay").fadeToggle(500);
+	$(".video-embed").fadeToggle(500);
+});
