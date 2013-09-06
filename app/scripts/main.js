a7b4d155983042a1d0b826b08c9d90f5dc8aa878
Will Dages <wdages@gmail.com>
2013-09-06 07:33:52 -0400

    Move page-specific scripts to their own pages, fix player hide/show functionality on homepage, start building up the application page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,7 @@
+/* jshint undef: true, unused: true */
+/* global fitText */
+
+'use strict';
+
 $(document).foundation();
 
-fitText(document.getElementById('hp-dollars-donated'), 0.72)
-
-var vimeoVideoId = '50959272'
-var vimeoPlayer = '<iframe src="http://player.vimeo.com/video/' + vimeoVideoId + '" frameborder="0" class="vimeo-video" autoplay webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
-
-$("dark-overlay, .play-button, .close-overlay, .video-embed").on('click', function () {
-	$(".video-embed").html($(vimeoPlayer));
-	$(".dark-overlay").fadeToggle(500);
-	$(".video-embed").fadeToggle(500);
-});
