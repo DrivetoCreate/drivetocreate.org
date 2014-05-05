d801f93f0403a71ec48b3388f38fef47ebaaa7b6
Will Dages <wdages@gmail.com>
2014-05-05 07:08:54 -0400

    Another iteration of the homepage

--- Gruntfile.js
+++ Gruntfile.js
@@ -251,6 +251,7 @@ module.exports = function (grunt) {
                         '*.{ico,png,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
+                        'videos/{,*/}*.{mp4,ogv,webm}',
                         'styles/fonts/*',
                         'application/*'
                     ]
